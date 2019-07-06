f4703235206570aeafc9773ac4cfdf340c71a40d
Muhammad Ewaily <muhammad.ewaily@gmail.com>
2019-07-06 12:08:45 +0200

    init project

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Muhammad Ewaily on 7/6/19.
+//  Copyright © 2019 Muhammad Ewaily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
